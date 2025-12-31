76dea11e4db5cb6867ce52564fd424232f3a3246
Robson Albuquerque <robalbuquerque98@gmail.com>
2025-12-31 17:30:48 -0300

    feat: add router formacao-liturgica

---
+++ src/app/formacao-liturgica/page.tsx
@@ -0,0 +1,10 @@
+import FormacaoLiturgicaLayout from "@/components/layout/FormacaoLiturgicaLayout";
+import FormacaoLiturgica from "@/components/FormacaoLiturgica";
+
+export default function FormacaoLiturgicaPage() {
+    return (
+        <FormacaoLiturgicaLayout>
+            <FormacaoLiturgica />
+        </FormacaoLiturgicaLayout>
+    );
+}
