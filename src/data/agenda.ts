0855f78c9c87b18418287798744decd84b97a9c3
Robson Albuquerque <robalbuquerque98@gmail.com>
2026-01-04 18:48:31 -0300

    update: folder data

--- src/data/agenda.ts
+++ src/data/agenda.ts
@@ -6,7 +6,7 @@ export interface AgendaItem {
     group: string;
     place: string;
     href?: string;
-    status: "active" | "coming-soon";
+    status: "active" | "coming-soon" | "completed";
 }
 
 export const agendaData: AgendaItem[] = [
@@ -15,22 +15,23 @@ export const agendaData: AgendaItem[] = [
         time: "19h",
         group: "Juventude de Santa Maria (JSM)",
         place: "Paróquia de Sant'Ana – Ribeirão",
-        href: "https://drive.google.com/drive/folders/1piiDYM-09UgReFzNS8ATcW_cZFPqXdRD?usp=sharing",
-        status: "active",
+        href: "https://drive.google.com/file/d/1U_2gMIF7Vui9MakX9yXHkSXax3ScHH2y/view?usp=sharing",
+        status: "completed",
     },
     {
         date: "03/01/2026",
         time: "19h",
         group: "Juventude de Santa Maria (JSM)",
         place: "Capela de São Pedro e São Paulo – Ribeirão",
-        href: "https://drive.google.com/SEU-LINK-AQUI",
-        status: "coming-soon",
+        href: "https://drive.google.com/drive/folders/1piiDYM-09UgReFzNS8ATcW_cZFPqXdRD?usp=sharing",
+        status: "completed",
     },
     {
         date: "11/01/2026",
         time: "19h",
         group: "Juventude de Santa Maria (JSM)",
         place: "Paróquia de Sant'Ana – Ribeirão",
+        href: "https://drive.google.com/file/d/1U_2gMIF7Vui9MakX9yXHkSXax3ScHH2y/view?usp=sharing",
         status: "coming-soon",
     },
 ];
